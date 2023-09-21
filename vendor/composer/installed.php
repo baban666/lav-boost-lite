1112dad34a12b0347b26c73db8c58bbe8ab24945
VladimirVeraksa <49755858+baban666@users.noreply.github.com>
2023-09-21 16:17:57 +0200

    Initial commit

---
+++ vendor/composer/installed.php
@@ -0,0 +1,24 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-main',
+    'version' => 'dev-main',
+    'aliases' => 
+    array (
+    ),
+    'reference' => '03945f2f3209ab9a68188adff01451d49bd584e9',
+    'name' => 'vladi/lav-boost',
+  ),
+  'versions' => 
+  array (
+    'vladi/lav-boost' => 
+    array (
+      'pretty_version' => 'dev-main',
+      'version' => 'dev-main',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '03945f2f3209ab9a68188adff01451d49bd584e9',
+    ),
+  ),
+);
